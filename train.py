36f51efb6ff7a6ced127c3935e94b9454631a706
abhaygargab <ab.abhaygarg96@gmail.com>
2022-03-28 23:51:10 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,163 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import pickle
+
+class LogoDataset(torch.utils.data.Dataset):
+    def __init__(self,transforms, dataset):
+#         self.root = root
+        self.transforms = transforms
+        with open(dataset, 'rb') as f:
+            self.dataset = pickle.load(f)
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir("/home/u1698461/Documents/ImpPersonalDocs/send/al/images")))
+#         self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+#         img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        img_path = self.dataset[idx][0]
+        img = Image.open(img_path).convert("RGB")
+
+        # get bounding box coordinates for each mask
+#         num_objs = len(obj_ids)
+        num_objs = 1
+        pos = self.dataset[idx][1]
+        boxes = []
+        for i in range(num_objs):
+#             pos = np.where(masks[i])
+            xmin = np.min(pos[0])
+            xmax = np.max(pos[2])
+            ymin = np.min(pos[1])
+            ymax = np.max(pos[3])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+#         masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img), target
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+    
+    
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+# load a model pre-trained on COCO
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# replace the classifier with a new one, that has
+# num_classes which is user-defined
+num_classes = 2  # 1 class (person) + background
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+
+# import transforms as T
+from torchvision import transforms as T
+import utils
+def get_transform(train):
+    transformations = []
+    transformations.append(T.ToTensor())
+    if train:
+        transformations.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transformations)
+
+
+# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+dataset_path = "/home/u1698461/Documents/ImpPersonalDocs/send/al/dataset.pkl"
+dataset = LogoDataset(get_transform(train=True), dataset_path)
+data_loader = torch.utils.data.DataLoader(
+ dataset, batch_size=2, shuffle=True, num_workers=4,
+ collate_fn=utils.collate_fn)
+# For Training
+images,targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images,targets)   # Returns losses and detections
+# For inference
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)           # Returns predictions
+
+
+
+from engine import train_one_epoch, evaluate
+import utils
+
+
+# train on the GPU or on the CPU, if a GPU is not available
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# our dataset has two classes only - background and person
+num_classes = 2
+# use our dataset and defined transformations
+dataset = LogoDataset(get_transform(train=True), dataset_path)
+dataset_test = LogoDataset(get_transform(train=False), dataset_path)
+
+# split the dataset in train and test set
+indices = torch.randperm(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[:-50])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+# define training and validation data loaders
+data_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=2, shuffle=True, num_workers=4,
+    collate_fn=utils.collate_fn)
+
+data_loader_test = torch.utils.data.DataLoader(
+    dataset_test, batch_size=1, shuffle=False, num_workers=4,
+    collate_fn=utils.collate_fn)
+
+# get the model using our helper function
+#     model = get_model_instance_segmentation(num_classes)
+
+# move model to the right device
+model.to(device)
+
+# construct an optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005,
+                            momentum=0.9, weight_decay=0.0005)
+# and a learning rate scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                               step_size=3,
+                                               gamma=0.1)
+
+
+# let's train it for 10 epochs
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+    # update the learning rate
+    lr_scheduler.step()
+    # evaluate on the test dataset
+    evaluate(model, data_loader_test, device=device)
+
+print("That's it!")
