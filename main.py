7c5663952ffdca0425c48d0f04739e3f1944125b
abhaygargab <ab.abhaygarg96@gmail.com>
2022-03-29 14:40:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import torch
 from PIL import Image
 from fastapi import FastAPI
+from torchvision.utils import save_image
+import numpy as np
+import matplotlib.pyplot as plt
 
 app = FastAPI()
 
@@ -13,6 +16,7 @@ def get_root():
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
+"""Loading the model for inference"""
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 num_classes = 2
 # get number of input features for the classifier
@@ -20,9 +24,11 @@ in_features = model.roi_heads.box_predictor.cls_score.in_features
 # replace the pre-trained head with a new one
 model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-checkpoint = torch.load('path/to/model/logo_detection.model')
-model.load_state_dict(checkpoint["model_state_dict"])
+checkpoint = torch.load('logo_detection.model')
+model.load_state_dict(checkpoint["state_dict"])
+model.eval()
 
+"""Preprocessing the images"""
 from torchvision import transforms as T
 
 def get_transform(train):
@@ -32,27 +38,28 @@ def get_transform(train):
         transformations.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transformations)
 
+
 def test(model, message):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     images = [get_transform(train=False)(Image.open(message).convert("RGB"))]
+    model.to(device)
     images = list(image.to(device) for image in images)
     predictions = model(images) 
     correct_preds = {'boxes': [], 'scores': []}
     for box, label, score in zip(predictions[0]['boxes'], predictions[0]['labels'], predictions[0]['scores']):
         if(score>0.7):
-            # correct_preds.append([box.cpu().detach().numpy(), score.cpu().detach().numpy()])
             correct_preds['boxes'].append(box.cpu().detach().numpy())
             correct_preds['scores'].append(score.cpu().detach().numpy())
-    # label = model.predict([message])[0]
-    # spam_prob = model.predict_proba([message])
-    return {'boxes': correct_preds['boxes'], 'scores': correct_preds['scores']}
+    
+    for idx, box in enumerate(correct_preds['boxes']):
+        im = images[0].permute(1,2,0).cpu().detach().numpy()[round(box[1]):round(box[3]), round(box[0]):round(box[2])]
+        plt.imshow(im)
+        plt.imsave(f'save_images/im_{idx}.png', im)
+        
+    return {"message": "Done : Saved the cropped portions in the save_images folder"}
 
 
-# @app.get('/logo_detection/')
-# async def detect_spam_query(message: str):
-#    return classify_message(model, message)
-
-
-@app.get('/query_image_path/{message}')
-async def detect_spam_path(message: str):
-   return test(model, message)
+@app.get('/logo_detection/')
+async def detect_spam_query(message: str):
+    # return {'message': f'Your path is {message}'}
+    return test(model, message)
